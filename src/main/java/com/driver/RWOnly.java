2bb96be849d7f9c975026e641dafd97165a53449
Sukruth Surendra N S <Sukruth.sns@campusuvce.in>
2023-03-19 18:55:21 +0530

    soln 1

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class RWOnly {
-    
+//    class named "RWOnly" with a private member variable named "name"
+    private String name;
+
+//    Implement getter and setter member functions for
+//    the "name" variable in the "RWOnly" class.
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
